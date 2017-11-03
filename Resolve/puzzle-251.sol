19e7f78ab7484665374bc343d400b4e3fab6b16a
LESSSE <luis.carlos.as@gmail.com>
2017-11-03 12:27:30 +0000

    Project done, missing readme

---
+++ Resolve/puzzle-251.sol
@@ -0,0 +1,9 @@
+8
+R d
+R r
+R d
+R l
+R d
+G u
+R r
+R u
---
+++ test.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+# usage: test.sh [test-files] 
+function debug { echo $@ >&2; }
+
+for f in `ls ./Problems/*.rr | sort -n -t - -k 2`; do
+	
+	base=$(basename "$f" ".rr")
+	echo "========================$f======================"
+	./proj1 "$f" > "Resolve/$base.sol" 2>"Resolve/$base.err"
+	./rr_checker "$f" "Resolve/$base.sol" 	
+	diff <(head -n 1 "Resolve/$base.sol") <(head -n 1 "Solutions/$base.sol")
+	
+done
+#debug "generated cube of size $size with <= $alive_n alive cells in $SECONDS seconds."
